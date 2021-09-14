910b7f2f4f9e4bc9232747646aef4e5771a50482
Andrei Oana <andreioana97@yahoo.com>
2021-09-14 22:25:04 +0300

    add dynamically generated data

--- Common/test_api_requests.py
+++ Common/test_api_requests.py
@@ -1,13 +1,14 @@
-from flask_restful.fields import Integer
+import time
+import random
 import requests
 from datetime import datetime
 
 BASE = "http://127.0.0.1:5000/"
-
-data = [{"date_created": datetime.now().strftime("%d %b %Y, %H:%M:%S"), "mega_bits_per_second": 0.50},
-        {"date_created": datetime.now().strftime("%d %b %Y, %H:%M:%S"), "mega_bits_per_second": 0.55},
-        {"date_created": datetime.now().strftime("%d %b %Y, %H:%M:%S"), "mega_bits_per_second": 0.90},
-        {"date_created": datetime.now().strftime("%d %b %Y, %H:%M:%S"), "mega_bits_per_second": 0.90}]
+"""
+data = [{"timestamp": datetime.now().strftime("%H:%M:%S"), "mega_bits_per_second": 0.50},
+        {"timestamp": datetime.now().strftime("%H:%M:%S"), "mega_bits_per_second": 0.55},
+        {"timestamp": datetime.now().strftime("%H:%M:%S"), "mega_bits_per_second": 0.90},
+        {"timestamp": datetime.now().strftime("%H:%M:%S"), "mega_bits_per_second": 0.90}]
 
 for i in range(len(data)):
     response = requests.put(BASE + "resource/" + str(i), data[i])
@@ -35,3 +36,19 @@ for i in range(len(data)):
 input()
 response = requests.get(BASE + "nr_resources")
 print("Tot nr of db recordings : " + (str)(response.json()['nr_resources']))
+"""
+
+data = []
+# build data list of json objects dynamically and add them in db
+# for i in range(10):
+#     time.sleep(5)
+#     json_obj = {}
+#     json_obj['timestamp'] = datetime.now().strftime("%d %b %Y, %H:%M:%S")
+#     json_obj['mega_bits_per_second'] = random.uniform(1.5, 10.9)
+#     response = requests.put(BASE + "resource/" + str(i), json_obj)
+#     data.append(json_obj)
+
+input()
+for i in range(10):
+    response = requests.get(BASE + "resource/" + str(i))
+    print(response.json())
